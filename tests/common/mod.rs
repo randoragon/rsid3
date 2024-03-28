f640f3c3cfabc2098e1628e842f2df757cb1f09c
Randoragon <randoragongamedev@gmail.com>
2024-03-28 22:11:02 +0100

    test: Add samples containing every frame type
    Prepared using mid3v2, hence they all contain ID3v2.4 tags.
    
    sample_0.mp3:    No tag whatsoever
    sample_TIT2.mp3: TIT2: "Sample Title"
    sample_TXXX.mp3: TXXX[Description] "Sample Content"
    sample_COMM.mp3: COMM[Description](eng) "Sample Content"
    
    Added associated constructors (via a new test_file_from_sample macro) to
    TestFile.

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,8 +4,14 @@ use std::fs::{create_dir_all, copy};
 use tempfile::NamedTempFile;
 use std::process::{Command, Output};
 
-/// Path to the directory containing preset samples.
-const SAMPLE_EMPTY: &str = "tests/samples/sample.mp3";
+/// Path to a sample MP3 file with no tags.
+const SAMPLE_EMPTY: &str = "tests/samples/sample_0.mp3";
+/// Path to a sample MP3 file with a TIT2 "Sample Title" tag.
+const SAMPLE_TIT2: &str = "tests/samples/sample_TIT2.mp3";
+/// Path to a sample MP3 file with a TXXX[Description] "Sample Content" tag.
+const SAMPLE_TXXX: &str = "tests/samples/sample_TXXX.mp3";
+/// Path to a sample MP3 file with a COMM[Description](eng) "Sample Content" tag.
+const SAMPLE_COMM: &str = "tests/samples/sample_COMM.mp3";
 /// Path to the directory for storing temporary files constructed and operated on in integration tests.
 const SAMPLES_TMPDIR: &str = "tests/samples/tmp/";
 /// Path to the rsid3 executable.
@@ -15,16 +21,25 @@ pub struct TestFile {
     file: NamedTempFile,
 }
 
-impl TestFile {
-    /// Creates a new tagless test file.
-    pub fn empty() -> Self {
-        create_dir_all(SAMPLES_TMPDIR).unwrap();
-        let file = NamedTempFile::new_in(SAMPLES_TMPDIR).unwrap();
-        copy(SAMPLE_EMPTY, file.path()).unwrap();
-        TestFile {
-            file,
+/// Defines a new constructor of `TestFile` which creates a copy of a given sample file.
+macro_rules! test_file_from_sample {
+    ($func_name:ident, $fpath:path) => {
+        pub fn $func_name() -> Self {
+            create_dir_all(SAMPLES_TMPDIR).unwrap();
+            let file = NamedTempFile::new_in(SAMPLES_TMPDIR).unwrap();
+            copy($fpath, file.path()).unwrap();
+            TestFile {
+                file,
+            }
         }
-    }
+    };
+}
+
+impl TestFile {
+    test_file_from_sample!(empty, SAMPLE_EMPTY);
+    test_file_from_sample!(tit2, SAMPLE_TIT2);
+    test_file_from_sample!(txxx, SAMPLE_TXXX);
+    test_file_from_sample!(comm, SAMPLE_COMM);
 
     /// Returns the path to the test file.
     pub fn path(&self) -> &Path {
