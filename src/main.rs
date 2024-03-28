b2eb9617672f5fa83db6ef54bfaba02165dfddaa
Randoragon <randoragongamedev@gmail.com>
2024-03-28 18:03:58 +0100

    feat: Handle tag conversion options
    The --id3v2.2, --id3v2.3, --id3v2.4, and their corresponding --force...
    variants are now supported and have been shown to work through
    rudimentary testing.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod cli;
 mod id3_helpers;
 
-use cli::Cli;
+use cli::{Cli, ConvertOpt};
 use id3_helpers::*;
 use std::process::ExitCode;
 use anyhow::{anyhow, Result};
-use id3::{Tag, TagLike, Frame};
+use id3::{Tag, TagLike, Frame, Version};
 
 /// Prints frames from a file, with a custom delimiter.
 fn print_file_frames(fpath: &str, frames: &Vec<Frame>, delimiter: &str) -> Result<()> {
@@ -88,8 +88,11 @@ fn print_all_file_frames_pretty(fpath: &str) -> Result<()> {
     Ok(())
 }
 
-// Writes frames into a file. Previous values are overwritten, if any.
-fn set_file_frames(fpath: &str, frames: Vec<Frame>) -> Result<()> {
+/// Writes frames into a file. Previous values are overwritten, if any.
+/// If `tag_version` is `None`, stick to the version in the existing tag.
+/// `force` dictates whether to force the conversion (omit frames which cannot be converted),
+/// or return an error if a lossless conversion is not possible.
+fn set_file_frames(fpath: &str, frames: Vec<Frame>, tag_version: Option<Version>, force: bool) -> Result<()> {
     let mut tag = match Tag::read_from_path(fpath) {
         Ok(tag) => tag,
         Err(e) => return Err(anyhow!("Failed to read tags from file '{fpath}': {e}")),
@@ -106,11 +109,29 @@ fn set_file_frames(fpath: &str, frames: Vec<Frame>) -> Result<()> {
         }
     }
 
-    if was_modified {
+    if was_modified || tag_version.is_some_and(|v| v != tag.version()) {
+        let tag_version = tag_version.unwrap();
+        if force {
+            tag = force_convert_tag(&tag, tag_version);
+        }
         try_write_tag(&tag, &fpath, tag_version)?;
     }
 
     Ok(())
+}
+
+/// Forcefully convert a tag to a target ID3 version. Any frames that do not exist in the
+/// target representation are simply omitted from the result.
+fn force_convert_tag(tag: &Tag, target_version: Version) -> Tag {
+    if tag.version() == target_version {
+        return tag.clone();
+    }
+
+    let mut new_tag = Tag::with_version(target_version);
+    for frame in tag.frames().filter(|x| x.id_for_version(target_version).is_some()) {
+        new_tag.add_frame(frame.clone());
+    }
+    new_tag
 }
 
 fn main() -> ExitCode {
@@ -143,6 +164,12 @@ fn main() -> ExitCode {
         cli.delimiter.clone().unwrap_or('\n'.to_string())
     };
 
+    // Only one convertion option at a time is allowed
+    if cli.convert_opts.len() > 1 {
+        eprintln!("rsid3: it is illegal to pass multiple convert options");
+        return ExitCode::FAILURE;
+    }
+
     // Handle all get options
     for fpath in &cli.files {
         if let Err(e) = print_file_frames(fpath, &cli.get_frames, &delimiter) {
@@ -151,11 +178,34 @@ fn main() -> ExitCode {
         }
     }
 
+    // Establish which tag version to write, and whether to force a conversion
+    let (tag_version, force_convert) = match cli.convert_opts.get(0) {
+        Some(ConvertOpt::Id3v22) => (Some(Version::Id3v22), false),
+        Some(ConvertOpt::Id3v23) => (Some(Version::Id3v23), false),
+        Some(ConvertOpt::Id3v24) => (Some(Version::Id3v24), false),
+        Some(ConvertOpt::Id3v22Force) => (Some(Version::Id3v22), true),
+        Some(ConvertOpt::Id3v23Force) => (Some(Version::Id3v23), true),
+        Some(ConvertOpt::Id3v24Force) => (Some(Version::Id3v24), true),
+        None => (None, false),
+    };
+
     // Handle all set options
     for fpath in &cli.files {
-        if let Err(e) = set_file_frames(fpath, cli.set_frames.to_owned()) {
+        if let Err(e) = set_file_frames(fpath, cli.set_frames.to_owned(), tag_version, force_convert) {
             eprintln!("rsid3: {e}");
             return ExitCode::FAILURE;
+        }
+    }
+
+    // Handle the convert option. This may be superfluous, if the tag has already been converted
+    // as a result of handling the set options above. But if no set options were passed, here is
+    // where the conversion happens.
+    if !cli.convert_opts.is_empty() {
+        for fpath in &cli.files {
+            if let Err(e) = set_file_frames(fpath, vec![], tag_version, force_convert) {
+                eprintln!("rsid3: {e}");
+                return ExitCode::FAILURE;
+            }
         }
     }
 
@@ -167,7 +217,8 @@ fn main() -> ExitCode {
         }
     }
 
-    if cli.get_frames.is_empty() && cli.set_frames.is_empty() && cli.del_frames.is_empty() {
+    if cli.get_frames.is_empty() && cli.set_frames.is_empty() && cli.del_frames.is_empty()
+    && cli.convert_opts.is_empty() && cli.purge_opts.is_empty() {
         if cli.files.is_empty() {
             Cli::print_usage();
             return ExitCode::FAILURE;
