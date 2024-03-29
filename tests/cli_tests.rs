90fa909e85fcc2f49f498038a0395f5086450804
Randoragon <randoragongamedev@gmail.com>
2024-03-30 00:47:05 +0100

    test: Extend prints_help() with "-h" variant
    Issue: #1

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -6,6 +6,10 @@ fn prints_help() {
     let output = rsid3_run(&["--help"]);
     assert!(output.status.success());
     assert!(output.stdout.starts_with("Usage:".as_bytes()));
+
+    let output = rsid3_run(&["-h"]);
+    assert!(output.status.success());
+    assert!(output.stdout.starts_with("Usage:".as_bytes()));
 }
 
 #[test]
