13b9870452f4736f0cd8b2e3f5a812298f42517c
Randoragon <randoragongamedev@gmail.com>
2024-03-30 00:47:45 +0100

    test: Add prints_version() test
    Issue: #1

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -13,6 +13,24 @@ fn prints_help() {
 }
 
 #[test]
+fn prints_version() {
+    let expected_prefix = &[
+        "rsid3 ".as_bytes(),
+        env!("RSID3_VERSION_STR").as_bytes(),
+        "-".as_bytes(),
+        env!("VERGEN_GIT_SHA").chars().take(8).collect::<String>().as_bytes(),
+    ].concat();
+
+    let output = rsid3_run(&["--version"]);
+    assert!(output.status.success());
+    assert!(output.stdout.starts_with(expected_prefix));
+
+    let output = rsid3_run(&["-V"]);
+    assert!(output.status.success());
+    assert!(output.stdout.starts_with(expected_prefix));
+}
+
+#[test]
 fn gets_empty() {
     let file = TestFile::empty();
     let output = rsid3_run(&[file.path()]);
