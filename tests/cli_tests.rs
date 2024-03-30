7e71d4e4b902bff1459992ad90e68f7c324d8a95
randoragon <randoragongamedev@gmail.com>
2024-03-30 01:15:39 +0100

    test: Add prints_supported_frames() test
    Added regex to dev-dependencies. Will definitely be handy for finding
    bytes substrings. Also move tempfile to dev-dependencies, because it was
    erroneously added to regular ones in c80b11f64441.
    
    Issue: #1

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -15,6 +15,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 mod common;
 use common::*;
+use regex::bytes::Regex;
 
 #[test]
 fn prints_help() {
@@ -43,6 +44,19 @@ fn prints_version() {
     let output = rsid3_run(&["-V"]);
     assert!(output.status.success());
     assert!(output.stdout.starts_with(expected_prefix));
+}
+
+#[test]
+fn prints_supported_frames() {
+    let re = Regex::new(
+        r"(?s)ID3 standard.*2\.2, 2\.3, 2\.4.*COMM.*TIT2.*r/w.*http://id3.org/"
+    ).unwrap();
+
+    let output = rsid3_run(&["--list-frames"]);
+    assert!(re.is_match(&output.stdout));
+
+    let output = rsid3_run(&["-L"]);
+    assert!(re.is_match(&output.stdout));
 }
 
 #[test]
