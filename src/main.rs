9958fb92dd7b88accf01246676a70aa76e174954
randoragon <randoragongamedev@gmail.com>
2026-08-18 13:57:13 +0200

    refactor: Move parts of main.rs into lib.rs
    This is more correct and tidy. Also, it enables us to import
    rsid3::ExitCode into tests, which is the main motivation.
    
    Issue: #1

--- src/main.rs
+++ src/main.rs
@@ -13,82 +13,10 @@
 // You should have received a copy of the GNU General Public License along
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-mod cli;
-mod id3_helpers;
-
-use cli::{Cli, Action, ConvertOpt, PurgeOpt};
-use std::path::Path;
-use id3_helpers::*;
-use anyhow::{anyhow, Result};
+use rsid3::*;
+use rsid3::id3_helpers::*;
+use rsid3::cli::{Cli, Action, PurgeOpt};
 use id3::{Tag, TagLike, Version};
-
-/// Exit codes used for various situations.
-#[derive(Debug)]
-enum ExitCode {
-    /// Process finished successfully.
-    Success = 0,
-
-    /// Attempted read of a frame that does not exist in a file. This exit code is important,
-    /// because it allows to determine that a frame does not exist, as opposed to it containing
-    /// an empty string.
-    FrameNotFound = 1,
-
-    /// The user passed incorrect arguments.
-    BadArg = 255,
-
-    /// Executing some action failed.
-    ActionFailed = 254,
-}
-impl std::process::Termination for ExitCode {
-    fn report(self) -> std::process::ExitCode {
-        match self {
-            ExitCode::Success => std::process::ExitCode::SUCCESS,
-            v => std::process::ExitCode::from(v as u8),
-        }
-    }
-}
-
-/// Pretty-prints all supported frames stored in the file.
-fn print_all_file_frames_pretty(fpath: &impl AsRef<Path>) -> Result<()> {
-    let tag = match Tag::read_from_path(fpath) {
-        Ok(tag) => tag,
-        Err(e) => match e.kind {
-            id3::ErrorKind::NoTag => {
-                eprintln!("{}: No tag found", fpath.as_ref().display());
-                return Ok(());
-            },
-            _ => return Err(anyhow!("Failed to read tag from file '{}': {e}", fpath.as_ref().display())),
-        }
-    };
-
-    let n_frames = tag.frames().count();
-    println!("{}: {}, {} frame{}:", fpath.as_ref().display(), tag.version(), n_frames,
-        if n_frames == 1 { "" } else { "s" });
-    for frame in tag.frames() {
-        print_frame_pretty(frame, tag.version())?;
-    }
-
-    Ok(())
-}
-
-/// Converts a tag according to the given command-line option.
-/// On success, returns whether any conversion happened (`false` iff the tag's version was already
-/// the same as the requested version).
-fn convert_tag(tag: &mut Tag, opt: ConvertOpt) -> Result<bool> {
-    let (tag_version, force) = match opt {
-        ConvertOpt::Id3v22 => (Version::Id3v22, false),
-        ConvertOpt::Id3v23 => (Version::Id3v23, false),
-        ConvertOpt::Id3v24 => (Version::Id3v24, false),
-        ConvertOpt::Id3v22Force => (Version::Id3v22, true),
-        ConvertOpt::Id3v23Force => (Version::Id3v23, true),
-        ConvertOpt::Id3v24Force => (Version::Id3v24, true),
-    };
-    if tag.version() == tag_version {
-        return Ok(false);
-    }
-    *tag = tag_with_version_from(tag, tag_version, force)?;
-    Ok(true)
-}
 
 fn main() -> ExitCode {
     let cli = match Cli::parse_args() {
